883f25bc19526da2a497cce5b477374e1d39f091
Salvador De Asis <dm18i04001@usonsonate.edu.sv>
2026-07-24 19:50:21 -0600

    Hacer dinámica la URL de redirección de MSAL para producción

--- frontend/src/config/authConfig.ts
+++ frontend/src/config/authConfig.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "9df8da80-7254-4065-9cc7-81e6aa8859ae",
     authority: "https://login.microsoftonline.com/058cc003-e5de-4ecb-a024-99bd0d50bbdd",
-    redirectUri: "http://localhost:5173/login", 
+    redirectUri: typeof window !== "undefined" ? window.location.origin + "/login" : "http://localhost:5173/login",
   },
   cache: {
     cacheLocation: "sessionStorage"
